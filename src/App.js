c63324395d10af8607ac1aeb1541c532fc25c42d
VARGHESE P ROY <125295191+Varghese254@users.noreply.github.com>
2024-11-04 15:28:42 +0530

    created an example for usestate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import './Register.css'
 import Signup from './Register';
 
 function App() {
-
- 
+  
   return (
     <div>
+      
       <Signup/>
     </div>
   );
--- src/Register.js
+++ src/Register.js
@@ -3,60 +3,28 @@ import './Register.css';
 
 function Register() {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: ''
+    name: ''
+    
   });
 
   const handleInputChanges = (event) => {
     const { name, value } = event.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
+    setFormData((prevData) => ({ ...prevData, [name]: value }));//shallow copy of the previous data...
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(formData, "data");
+    setFormData({ name: 'default' });
   };
 
   return (
     <div className='div'>
       <h1>REGISTRATION</h1>
       <form onSubmit={handleSubmit}>
-        <input 
-          type="text" 
-          id="name" 
-          name="name" 
-          placeholder='Name' 
-          value={formData.name} 
-          onChange={handleInputChanges} 
-          required 
-        />
-        <br/>
-        <input 
-          type="email" 
-          id="email" 
-          name="email" 
-          placeholder='Email' 
-          value={formData.email} 
-          onChange={handleInputChanges} 
-          required 
-        />
-        <br/>
-        <input 
-          type="password" 
-          id="password" 
-          name="password" 
-          placeholder='Password' 
-          value={formData.password} 
-          onChange={handleInputChanges} 
-          required 
-        />
-        <br/>
-        <div className="button-container">
-          <button type="submit" onChange={(event)=>handleSubmit(event)}>Register</button>&nbsp;
-          <button type="button" >Cancel</button>
-        </div>
-        <br/>
+      <h1>Sate Handling-{formData.name}</h1>
+      <input type="text" name="name" value={formData.name} 
+          onChange={handleInputChanges} />
+      <button onChange={(event)=>handleSubmit(event)}>Submit</button>
       </form>
     </div>
   );
